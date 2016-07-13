dc4fdb0af028a0cbdcfb4c2422e43503201410ed
Joseph Hwang <jzh@fb.com>
2016-07-13 09:38:37 -0700

    HBASE-16209 Exponential retry policy for Assignmanager failed_open retries

--- hbase-server/src/main/java/org/apache/hadoop/hbase/master/RegionStates.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/master/RegionStates.java
@@ -21,6 +21,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
